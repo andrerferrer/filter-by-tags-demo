7ab6ba9d9dd6b2979c11bddeffe20f7fbdb91968
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-16 15:11:00 -0300

    Validates that a restaurant can't have the same tag twice (and vice-versa)

--- app/models/restaurants_tag.rb
+++ app/models/restaurants_tag.rb
@@ -1,4 +1,6 @@
 class RestaurantsTag < ApplicationRecord
   belongs_to :restaurant
   belongs_to :tag
+  # Validates that a restaurant can't have the same tag twice (and vice-versa)
+  validates_uniqueness_of :restaurant, scope: [:tag]
 end
